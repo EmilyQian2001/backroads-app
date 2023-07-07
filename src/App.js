32ed894aa979d40cf1dec4ff8e08920d39cf10fa
Emily <yichun@EmilydeMacBook-Pro.local>
2023-07-07 10:55:37 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero"
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Tours from "./components/Tours";
+import Service from "./components/Service";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Service />
+      <Tours />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
